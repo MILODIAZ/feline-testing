89513a1c3de8173646fdf343f172ecb044a1cb65
JuanNilo <90728553+JuanNilo@users.noreply.github.com>
2023-05-25 15:36:25 -0400

    $

--- src/Components/ProductContainer.js
+++ src/Components/ProductContainer.js
@@ -29,7 +29,7 @@ function ProductContainer (props) {
               {props.description} 
             </p>
             <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-gray-900">
-              Precio: {props.price}
+              Precio: ${props.price}
             </h5>
             <p
               class="mb-3 font-normal text-gray-700 dark:text-gray-700">
