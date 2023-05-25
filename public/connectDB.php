51a084ffba9c22cc001fd2183d26d5fa5cb3dd2d
Joshir Contreras Segovia <joshir.contreras@gmail.com>
2023-05-24 22:51:16 -0400

    Se realizo cambios en la fuente y base de datos

--- public/connectDB.php
+++ public/connectDB.php
@@ -1,17 +1,15 @@
 <?php
 
-    $host = "localhost";
-    $dbname = "INGESOTESTING";
-    $username = "postgres";
-    $password = "reddead";    
+$host = "localhost";
+$dbname = "INGESOTESTING";
+$username = "postgres";
+$password = "1234";
 
-    try{
-        $conn = new PDO ("pgsql:host = $host; dbname=$dbname", $username, $password);
-        /*echo "Se conectó correctamente a la Base de Datos";*/
-    }
-    
-    catch(PDOException $exp){
-     /*echo ("No se pudo conectar a la base de datos, $exp");*/
-    }
-    
+try {
+    $conn = new PDO("pgsql:host = $host; dbname=$dbname", $username, $password);
+    /*echo "Se conectó correctamente a la Base de Datos";*/
+} catch (PDOException $exp) {
+    /*echo ("No se pudo conectar a la base de datos, $exp");*/
+}
+
 ?>
