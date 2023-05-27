c96b600ad3e8eb7c2b0b519afb523c1ef9f7d3d9
JuanNilo <90728553+JuanNilo@users.noreply.github.com>
2023-05-27 01:11:24 -0400

    No BG No Products

--- src/Components/NoProducts.js
+++ src/Components/NoProducts.js
@@ -4,8 +4,8 @@ import { useAuthContext } from "../Contexts/AuthContext";
 function NoProducts() {
 
     return (
-        <div className="drop-shadow-2xl inset-0 z-10">
-            <div className=" w-[100%] mx-auto rounded-lg shadow-xl bg-teal-300 sm:max-w-[500px]">
+        <div className=" inset-0 z-10">
+            <div className=" w-[100%] mx-auto rounded-lg   sm:max-w-[500px]">
                     <img className="  mx-auto drop-shadow-2xl" src={require('../images/gato-sad.png')} alt='no hay productos'/>
                 <div className="pb-6 text-center" >
                     <h5
