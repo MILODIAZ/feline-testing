94fbb2cbcc2ee803576a7d895d8e9c3ad255f7ec
JuanNilo <90728553+JuanNilo@users.noreply.github.com>
2023-05-30 10:51:39 -0400

    x

--- src/PrivateComponents/Inventario.js
+++ src/PrivateComponents/Inventario.js
@@ -50,22 +50,22 @@ function Inventario() {
     const [productName, setProductName] = useState('');
     const [productStock, setProductStock] = useState(0);
 
-    const handleOpenStock = (code, name, stock) => {        
-        if (openStock){
+    const handleOpenStock = (code, name, stock) => {
+        if (openStock) {
             setOpenStock(false);
             setProductCode('');
-            setProductStock(0);                    
+            setProductStock(0);
         } else {
             setOpenStock(true);
             setProductCode(code);
             setProductStock(stock);
-            setProductName(name);          
-        }               
-    }   
+            setProductName(name);
+        }
+    }
 
     return (
         <div>
-            {openStock? <SetStock stock={productStock} codigo={productCode} name={productName} handleClick={handleOpenStock} reloadProducts={reloadProducts} /> : null}
+            {openStock ? <SetStock stock={productStock} codigo={productCode} name={productName} handleClick={handleOpenStock} reloadProducts={reloadProducts} /> : null}
             <div>
                 {/*  htmlFor={categoryFilterID} */}
                 <label>Filtro categoria</label>
@@ -79,79 +79,87 @@ function Inventario() {
             </div>
             {/* Lista de productos */}
             <div className="lg:p-8 rounded-md w-[100%]">
-                <ScrollToTopButton/>
+                <ScrollToTopButton />
                 <div className=" flex items-center  justify-between pb-6">
                     <div>
                         <div className="lg:-mx-4 w-[100%]  px-4 sm:px-8  overflow-x-auto">
                             <div className="inline-block  shadow rounded-lg overflow-hidden">
                                 <table className='min-w-full leading-normal'>
-                                    {dataProductLoaded?
-                                    <tbody>
-                                        {products.map(product => (
-                                            <tr key={product[0]} className={`px-[10px] items-center border-solid border-2 border-gray-400 w-[100%] h-[150px] inline-flex mt-2 xs:mt-0
+                                    {dataProductLoaded ?
+                                        <tbody>
+                                            {products.map(product => (
+                                                <tr key={product[0]} className={`px-[10px] items-center border-solid border-2 border-gray-400 w-[100%] h-[150px] inline-flex mt-2 xs:mt-0
                                             
                                             ${(() => {
-                                                    switch (true) {
-                                                        case product[5] >= product[6]:
-                                                            return 'bg-[#00ff00]';
-                                                        case product[5] < product[7]:
-                                                            return 'bg-[#dd7e6b]';
-                                                        default:
-                                                            return 'bg-[#fff2cc]';
-                                                    }
-                                                })()}
+                                                        switch (true) {
+                                                            case product[5] >= product[6]:
+                                                                return 'bg-[#00ff00]';
+                                                            case product[5] < product[7]:
+                                                                return 'bg-[#dd7e6b]';
+                                                            default:
+                                                                return 'bg-[#fff2cc]';
+                                                        }
+                                                    })()}
                                             
                                             `}>
-                                                {/* Codigo */}
-                                                <td className='w-[12.5%]  w-1/2 py-5 px-2 lg:px-5 border border-gray-900 bg-white text-md lg:text-xl bold text-center '>
-                                                    <p className='text-gray-900 whitespace-no-wrap'>
-                                                        {product[0]}
-                                                    </p>
-                                                </td>
-                                                {/* Nombre e imagen */}
-                                                <td className='w-2/4 px-5 py-5 border-gray-200 text-sm'>
-                                                    <div className='flex items-center'>
-                                                        <div className='w-1/2  flex-shrink-0'>
-                                                            <img alt='product' className='h-[120px] lg:h-[140px]  min-w-1/2' src={require(`../productsImages/${product[0]}.jpg`)} />
+                                                    {/* Nombre e imagen */}
+                                                    <td className='w-[40%] px-5 py-5 border-gray-200 text-sm'>
+                                                        <div className='flex items-center'>
+                                                            <div className='w-1/2  flex-shrink-0'>
+                                                                <img alt='product' className='h-[140px] lg:h-[140px]  min-w-1/2' src={require(`../productsImages/${product[0]}.jpg`)} />
+                                                            </div>
+                                                            <div className='w-1/2 ml-3 text-lg md:text-xl lg:text-2xl'>
+                                                                <p className='text-sm md:text-md lg:text-lg text-gray-900  whitespace-no-wrap'>
+                                                                    {product[0]}
+                                                                </p>
+                                                                <h2 className='text-gray-900 font-bold whitespace-no-wrap'>
+                                                                    {product[2]}
+                                                                </h2>
+                                                            </div>
                                                         </div>
-                                                        <div className='w-1/2 ml-3 text-xl lg:text-2xl'>
-                                                            <h2 className='text-gray-900 whitespace-no-wrap'>{product[2]}</h2>
-                                                        </div>
-                                                    </div>
-                                                </td>
+                                                    </td>
 
-                                                {/* Stock */}
-                                                <td className='w-1/4 px-5 py-5 border-gray-200 text-xl lg:text-2xl'>
-                                                    <p className='text-gray-900 whitespace-no-wrap'>
-                                                        Stock: {product[5]}
-                                                    </p>
-                                                </td>
-                                                {/* Botones */}
-                                                <td>
-                                                    <button
-                                                        onClick={() => handleOpenStock(product[0], product[2], product[5])}
-                                                        className="text-sm text-white transition duration-150 hover:bg-indigo-900 bg-blue-600 font-bold py-2 px-4 rounded-l">
-                                                        Actualizar Stock
-                                                    </button>
-                                                </td>
+                                                    {/* Stock */}
+                                                    <td className='w-[40%] px-5 py-5 border-gray-200'>
+                                                        <p className='text-md md:text-xl lg:text-2xl font-bold text-gray-900 whitespace-no-wrap'>
+                                                            Stock: {product[5]}
+                                                        </p>
+                                                        <p className='sm:text-md lg:text-lg text-gray-900 whitespace-no-wrap'>
+                                                            Recomendado: {product[6]}
+                                                        </p>
+                                                        <p className='sm:text-md lg:text-lg text-gray-900 whitespace-no-wrap'>
+                                                            bajo: {product[7]}
+                                                        </p>
 
-                                                <td>
+                                                    </td>
 
-                                                    <button
-                                                        className="text-sm text-black transition duration-150 hover:bg-yellow-700 bg-yellow-500 font-bold py-2 px-4">
-                                                        Modificar Producto
-                                                    </button>
-                                                </td>
-                                                
-                                                <td>
-                                                    <button
-                                                        className="text-sm text-white transition duration-150 hover:bg-red-900 bg-red-600 font-bold py-2 px-4 rounded-r">
-                                                        Eliminar Producto
-                                                    </button>
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody> : null }
+
+                                                    {/* Botones */}
+                                                    <td>
+                                                        <button
+                                                            onClick={() => handleOpenStock(product[0], product[2], product[5])}
+                                                            className="text-sm text-white transition duration-150 hover:bg-indigo-900 bg-blue-600 font-bold py-2 px-4 rounded-l">
+                                                            Actualizar Stock
+                                                        </button>
+                                                    </td>
+
+                                                    <td>
+
+                                                        <button
+                                                            className="text-sm text-black transition duration-150 hover:bg-yellow-700 bg-yellow-500 font-bold py-2 px-4">
+                                                            Modificar Producto
+                                                        </button>
+                                                    </td>
+
+                                                    <td>
+                                                        <button
+                                                            className="text-sm text-white transition duration-150 hover:bg-red-900 bg-red-600 font-bold py-2 px-4 rounded-r">
+                                                            Eliminar Producto
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody> : null}
 
                                 </table>
                             </div>
