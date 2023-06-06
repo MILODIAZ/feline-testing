2192719739e72f20f4bfa6aec7a52b501b3d840a
JuanNilo <90728553+JuanNilo@users.noreply.github.com>
2023-06-05 22:01:06 -0400

    Filtro Lotes

--- src/PrivateComponents/SeguimientoPedidos.js
+++ src/PrivateComponents/SeguimientoPedidos.js
@@ -5,7 +5,8 @@ function SeguimientoPedidos () {
 
   const [lotesData, setLotesData ] = useState([]);
   const [dataLoaded, setDataLoaded] = useState(true);
-
+  const [filtroRetraso, setFiltroRetraso] = useState('todos');
+  
   useEffect(() => {
     loadLotes();
   },[]);
@@ -20,14 +21,46 @@ function SeguimientoPedidos () {
       .catch(error => console.log(error));
   }
 
+  const handleFiltroChange = (event) => {
+    setFiltroRetraso(event.target.value);
+  };
+
+  const filtroLotesPorRetraso = (lotes) => {
+    switch(filtroRetraso){
+      case 'atrasado':
+        return lotes.filter(lote => lote[4] < 0);
+      case 'sin-retraso':
+        return lotes.filter(lote => lote[4] >= 3);
+      default:
+        return lotes;
+    }
+  }
+
+  const lotesFiltrados = filtroLotesPorRetraso(lotesData);
   return(
     <div className='xl:px-40'>
       <div className='py-4 pl-10'>
         <p className='text-xl font-bold'>SEGUIMIENTO DE PEDIDOS</p>
       </div>
+      
+      <div  className='px-8 grid grid-cols-2 md:grid-cols-8 xl:grid-cols-8 gap-2 mt-4'>
+        <label htmlFor="filtroRetraso" className="mr-2">
+          <h5 className="text-xl">Filtrar por retraso:</h5>
+        </label>
+        <select
+          className=' px-4 py-2 w-[80%] rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-md' 
+          id="filtroRetraso"
+          value={filtroRetraso}
+          onChange={handleFiltroChange}
+        >
+          <option value="todos">Todos</option>
+          <option value="atrasado">Atrasado</option>
+          <option value="sin-retraso">Sin retraso</option>
+        </select>
+      </div>
 
       {dataLoaded ? 
-        lotesData.map(lote => (                    
+        lotesFiltrados.map(lote => (                    
           <LoteCard id={lote[0]} proveedor={lote[1]==null? 'SIN PROVEEDOR' : lote[1]} fecha_pedido={lote[2]} fecha_llegada={lote[3]} dias_restantes={lote[4]} />                     
         )) : <h1>Cargando...</h1>}        
     </div>
