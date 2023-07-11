fecd8f6d99a464a287bf7bf215905302807fa93b
JuanNilo <juan.nilo@alumnos.ucn.cl>
2023-07-11 15:17:56 -0400

    Modal de eliminacion

---
+++ src/PrivateComponents/Extras/ModalConfirm.js
@@ -0,0 +1,24 @@
+export default function ConfirmationModal({ message, onConfirm, onCancel }) {
+    return (
+      <div className="fixed z-50 inset-0 flex items-center justify-center">
+        <div className="bg-white p-8 rounded-[15px] border-solid border-[#000] border-[3px]">
+          <h2 className="text-lg font-medium mb-4">Confirmar eliminación</h2>
+          <p>{message}</p>
+          <div className="mt-4 flex justify-end">
+            <button
+              className="px-4 py-2 mr-2 bg-[#fc7494] font-bold rounded hover:bg-[#FA567C]"
+              onClick={onConfirm}
+            >
+              Eliminar
+            </button>
+            <button
+              className="px-4 py-2 bg-[#54e9d1] font-bold rounded hover:bg-teal-500"
+              onClick={onCancel}
+            >
+              Cancelar
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
--- src/PrivateComponents/Lote/Products-Lote.js
+++ src/PrivateComponents/Lote/Products-Lote.js
@@ -1,19 +1,25 @@
 import { FaTimes } from "react-icons/fa";
+import { useState } from "react";
+import ConfirmationModal from "../Extras/ModalConfirm";
 
 function ProductoLote(props)  {
 
+    const [showConfirmation, setShowConfirmation] = useState(false);
+
     const eliminarProducto = () => {
+        setShowConfirmation(false);
         const idLote = props.idLote;
         fetch(`http://localhost/feline-testing/public/main.php?query=25&lote=${idLote}&codProducto=${props.id}`)
-          .then((response) => response.json())
-          .then((data) => {
+        .then((response) => response.json())
+        .then((data) => {
             if (data) {
-              alert('Producto eliminado del lote');
-              props.cargarProductos();
+            alert('Producto eliminado del lote');
+            props.cargarProductos();
             }
-          })
-          .catch((error) => console.log(error));
-      };
+        })
+        .catch((error) => console.log(error));
+            
+        };
 
     return(
         <div key={props.id} 
@@ -26,9 +32,17 @@ function ProductoLote(props)  {
                              {/* Stock enviado */}
                              <input className="w-[70%] bg-gray-100 text-gray-800 border border-gray-300 rounded-md px-4 py-2 hover:bg-white" type="number" defaultValue={props.stockEnviado} />
                             </div>
-                            <button onClick={eliminarProducto}>
+                            <button onClick={() => setShowConfirmation(true)}>
                                 <FaTimes className='text-3xl hover:text-[#a5d5d5] transition-all'/>
                             </button>
+                            {showConfirmation && (
+                                <ConfirmationModal
+                                message="¿Estás seguro de que deseas eliminar el producto?"
+                                onConfirm={eliminarProducto}
+                                onCancel={() => setShowConfirmation(false)}
+                                />
+                            
+                        )}
         </div>
     )
 }
