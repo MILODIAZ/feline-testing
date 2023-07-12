b5cd1b128da4565a5f82283e28bdfda48836650b
JuanNilo <juan.nilo@alumnos.ucn.cl>
2023-07-12 18:42:41 -0400

    little error

--- src/PrivateComponents/Lote/Header-Lote.js
+++ src/PrivateComponents/Lote/Header-Lote.js
@@ -4,10 +4,10 @@ function HeaderLote(props) {
     return (
         <div className="text-center justify-center mb-[20px] items-center ">
             <div className=" bg-[#fc7494] font-bold border-solid border-[2px] border-[#000] rounded-[10px] ">
-                <div class=" text-xl mb-[20px] ">
+                <div className=" text-xl mb-[20px] ">
                     <p>Numero de lote: {props.id}</p> {/* Código del lote */}
                 </div>
-                <div class="grid grid-cols-3 h-[70%] mb-[20px]">
+                <div className="grid grid-cols-3 h-[70%] mb-[20px]">
                     <input className="bg-[#fc7494] m-auto" type="date" defaultValue={props.fechaPedido} />
                     <input className="bg-[#fc7494]  m-auto" type="date" defaultValue={props.fechaLlegada} />
                     <p>Días de retrasos: {props.diasRestantes}</p>
