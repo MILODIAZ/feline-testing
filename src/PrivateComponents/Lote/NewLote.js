4612c99d2bd233e9383c1cddab8126c1b413565f
JuanNilo <juan.nilo@alumnos.ucn.cl>
2023-07-19 17:10:09 -0400

    Nuevo lote terminado

--- src/PrivateComponents/Lote/NewLote.js
+++ src/PrivateComponents/Lote/NewLote.js
@@ -10,6 +10,10 @@ function NewLote(props) {
     const [fechaPedido, setFechaPedido] = useState([]);
     const [fechaLlegada, setFechaLlegada] = useState([]);
     const [showAlert, setShowAlert] = useState(false);
+    const [showErrorAlert, setShowErrorAlert] = useState(false);
+    const defaultProveedorValue = 'Sin proveedor';
+    const [selectedProveedor, setSelectedProveedor] = useState(defaultProveedorValue);
+    const [mensaje, setMensaje] = useState('');
 
     useEffect(() => {
         dataProviders();
@@ -29,18 +33,69 @@ function NewLote(props) {
             .catch(error => console.log(error));
     };
 
+    const [fieldErrors, setFieldErrors] = useState({});
+
+  // Function to validate form fields
+  const validateForm = () => {
+    const errors = {};
+
+    if (!codigo) {
+      errors.codigo = "Ingresa el código del lote.";
+    }
+
+    if (!fechaPedido) {
+        errors.fechaPedido = "Ingresa la fecha de pedido.";
+      } else {
+        // Validate fechaPedido is a valid date
+        const datePedido = new Date(fechaPedido);
+        if (isNaN(datePedido)) {
+          errors.fechaPedido = "Fecha de pedido inválida.";
+        }
+      }
+
+      if (!fechaLlegada) {
+        errors.fechaLlegada = "Ingresa la fecha de llegada.";
+      } else {
+        // Validate fechaLlegada is a valid date
+        const dateLlegada = new Date(fechaLlegada);
+        if (isNaN(dateLlegada)) {
+          errors.fechaLlegada = "Fecha de llegada inválida.";
+        }
+      }
+  
+    if (selectedProveedor === defaultProveedorValue) {
+      errors.proveedor = "Selecciona un proveedor válido.";
+    }
+
+    return errors;
+  };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        fetch(`http://localhost/feline-testing/public/main.php?query=27&codigo=${codigo}&nombre_proveedor=${nombreProveedor}&fecha_pedido=${fechaPedido}&fecha_llegada=${fechaLlegada}`)
+
+        // Validate form fields
+    const errors = validateForm();
+
+    // If there are errors, display the error messages
+    if (Object.keys(errors).length > 0) {
+        setMensaje('Por favor, complete todos los campos obligatorios.')
+        setShowErrorAlert(true);
+        return;
+      }
+        fetch(`http://localhost/feline-testing/public/main.php?query=27&codigo=${codigo}&nombre_proveedor=${selectedProveedor}&fecha_pedido=${fechaPedido}&fecha_llegada=${fechaLlegada}`)
             .then(response => response.json())
             .then(data => {
                 if (data === true) {
                     console.log(data)
                 }
+                
                 setShowAlert(true);
                 props.reloadLotes(true);
-            });
+            }).catch((e) => {
+                setShowErrorAlert(true)
+                    setMensaje('Error al ingresar lote, revise denuevo los campos');
+  
+            })
     };
     const handleConfirm = () => {
         setShowAlert(false);
@@ -77,14 +132,19 @@ function NewLote(props) {
 
                             <div className='flex flex-row justify-between pb-2'>
                                 <label htmlFor="proveedor">Proveedor:</label>
-                                <select id="proveedor" name="proveedor" onChange={(e) => setNombreProveedor(e.target.value)}>
+                                <select id="proveedor" name="proveedor" onChange={(e) =>setSelectedProveedor(e.target.value)} value={selectedProveedor}>
+                                    <option value={defaultProveedorValue}>{defaultProveedorValue}</option>
                                     {providers.map((provider) => (
                                         <option key={provider[0]} value={provider[0]}>
                                             {provider[0]}
                                         </option>
                                     ))}
                                 </select>
+    
                             </div>
+                            {showErrorAlert && (
+      <AlertConfirm mensaje={mensaje} onCancel={() => setShowErrorAlert(false)} />
+    )}
                             <button
                                 className="bg-[#54e9d1] w-[150px] font-bold h-[50px] mx-auto flex items-center justify-center border-solid border-[2px] border-[#000] rounded-[10px] mb-[20px]"
                                 type="submit">Crear Lote</button>
